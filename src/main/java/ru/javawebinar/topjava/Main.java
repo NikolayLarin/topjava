f6543bace2b194bc796cfbcff3951672ebd21f67
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-10-03 14:35:48 +0300

    HW_00. Runnable, Consumer

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,11 +1,28 @@
 package ru.javawebinar.topjava;
 
+import java.util.function.Consumer;
+
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo</a>
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+        execute(() -> {
+            System.out.println("Hello Topjava Enterprise!");
+        });
+        consume(System.out::println, "Hello Topjava Enterprise!");
+    }
+
+    private static void execute(Runnable runnable) {
+        System.out.println("Start runner");
+        runnable.run();
+        System.out.println("End runner");
+    }
+
+    private static <String> void consume(Consumer<String> consumer, String out) {
+        System.out.println("Start runner");
+        consumer.accept(out);
+        System.out.println("End runner");
     }
 }
