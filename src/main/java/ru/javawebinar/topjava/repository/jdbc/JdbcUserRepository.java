7169480e011817339964e93650d0710c5229862b
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-12-10 16:26:48 +0200

    HW_09 Meals Bean Validation

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -49,7 +49,6 @@ public class JdbcUserRepository implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-            insertRoles(user);
         } else {
             if (namedParameterJdbcTemplate.update(
                     "UPDATE users SET name=:name, email=:email, password=:password, " +
@@ -60,8 +59,8 @@ public class JdbcUserRepository implements UserRepository {
             // More complicated : get user roles from DB and compare them with user.roles (assume that roles are changed rarely).
             // If roles are changed, calculate difference in java and delete/insert them.
             deleteRoles(user);
-            insertRoles(user);
         }
+        insertRoles(user);
         return user;
     }
 
